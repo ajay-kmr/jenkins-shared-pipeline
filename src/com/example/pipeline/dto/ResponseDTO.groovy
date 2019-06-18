4e98a171334a017d5109c083b12c66a6aaeff7ba
Ajay Kumar <ajay.ishan06@gmail.com>
2019-06-18 23:19:45 +0530

    Initial set up for shared pipeline- WIP

--- src/com/example/pipeline/dto/ResponseDTO.groovy
+++ src/com/example/pipeline/dto/ResponseDTO.groovy
@@ -1,11 +1,17 @@
 package com.example.pipeline.dto
 
-
-import groovy.transform.ToString
-
-@ToString(includeNames = true, includeFields = true)
 class ResponseDTO<T> {
     boolean status
     String message
     T data
+
+
+    @Override
+    String toString() {
+        return "ResponseDTO{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}'
+    }
 }
