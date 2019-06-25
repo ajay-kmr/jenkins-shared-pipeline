f47b105f32dcea7cf4513cdc3697b5db84e6ede4
Ajay Kumar <ajay.ishan06@gmail.com>
2019-06-25 14:44:42 +0530

    Updated BuildTool enum

--- vars/buildSingleModuleApplication.groovy
+++ vars/buildSingleModuleApplication.groovy
@@ -4,5 +4,7 @@ import com.example.pipeline.stage.runner.PipeLineStageRunner
 def call(Closure buildConfig) {
     echo "******************** Building project using script:- buildSingleModuleApplication ********************"
     BuildRequestDetails buildRequest = BuildRequestDetails.getInstance(buildConfig)
+    echo "BuildRequestDetails:- "
+    echo buildRequest.toString()
     PipeLineStageRunner.run(this, buildRequest)
 }
