41b10205b88b2c53c9258246c12d7b504f0e6e00
Ajay Kumar <ajay.ishan06@gmail.com>
2019-06-30 01:37:51 +0530

    Updated code to publish HTML report

--- src/com/example/pipeline/stage/java/gradle/Build.groovy
+++ src/com/example/pipeline/stage/java/gradle/Build.groovy
@@ -20,7 +20,7 @@ class Build extends PipeLineStageImpl<String> {
                 echo "Running stage ${stageName}.."
                 unStash Stage.PREPARE.displayName
                 sh "./gradlew clean build"
-                stash name: stageName, useDefaultExcludes: false
+                stash name: stageName
                 responseDTO.stashName = stageName
                 stageStatus = StageStatus.SUCCESS
             }
